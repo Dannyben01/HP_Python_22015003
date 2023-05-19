92c7ef0839cad82322ccac58192227e32c8faa2e
Dannyben01 <133768417+Dannyben01@users.noreply.github.com>
2023-05-19 10:25:33 +0100

    Create Python_basic_1.py

---
+++ Python_basic_1.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+
+age = input("Enter your age: ")
+
+phone_number = input("Enter your phone number: ")
+
+print("Hello " + name + ", you are " + age + " years old with the phone number " + phone_number)
