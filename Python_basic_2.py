17b434b0bd2266e9f2e1d93943a14571143dd40d
Dannyben01 <133768417+Dannyben01@users.noreply.github.com>
2023-05-19 10:27:09 +0100

    Create Python_basic_2.py

---
+++ Python_basic_2.py
@@ -0,0 +1,25 @@
+# Initialize variables a and b
+
+a = 6
+
+b = 8
+
+# Print the initial values
+
+print("Before swapping:")
+
+print("a =", a)
+
+print("b =", b)
+
+# Swap the values
+
+a, b = b, a
+
+# Print the swapped values
+
+print("After swapping:")
+
+print("a =", a)
+
+print("b =", b)
