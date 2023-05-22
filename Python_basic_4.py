812338b48807318cb8ccfa86c90256b0e06c8f32
Dannyben01 <133768417+Dannyben01@users.noreply.github.com>
2023-05-22 11:07:50 +0100

    Create Python_basic_4.py

---
+++ Python_basic_4.py
@@ -0,0 +1,17 @@
+for i in range(1, 51):
+
+    if i % 3 == 0 and i % 5 == 0:
+
+        print(''couple'')
+
+    elif i % 3 == 0:
+
+        print(''boy'')
+
+    elif i % 5 == 0:
+
+        print(''girl'')
+
+    else:
+
+        print(i)
